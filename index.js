8ff271de1734f5ad2fb1a33385c35dd44311084d
pi <pi@DebianDev.lan>
2026-05-22 19:21:46 +0200

    Convert headingTrue radians to degrees

--- index.js
+++ index.js
@@ -144,7 +144,8 @@ module.exports = function(app) {
           // Get boat position from Signal K
           const boatLat = app.getSelfPath('navigation.position.value.latitude') || null;
           const boatLon = app.getSelfPath('navigation.position.value.longitude') || null;
-          const boatHeading = app.getSelfPath('navigation.headingTrue.value') || 0;
+          const headingTrue = app.getSelfPath('navigation.headingTrue.value');
+          const boatHeading = typeof headingTrue === 'number' ? radiansToDegrees(headingTrue) : 0;
 
           // Enrich detections with GPS position
           const enriched = detections.map(d => {
@@ -208,4 +209,8 @@ function redactRtspUrl(rtspUrl) {
   } catch (err) {
     return rtspUrl;
   }
+}
+
+function radiansToDegrees(value) {
+  return value * 180 / Math.PI;
 }
